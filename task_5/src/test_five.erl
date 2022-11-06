008b12b1403431161739a79599e2cad59d0a18eb
олег <oleg.ru1234567890@mail.ru>
2022-11-06 12:30:10 +0300

    move file

--- task_5/src/test_five.erl
+++ task_5/src/test_five.erl
@@ -4,13 +4,12 @@
 %% API
 -include_lib("eunit/include/eunit.hrl").
 
--export([test/0]).
-
 
 %% test five module
-test() ->
+funct_test() ->
   ?assertEqual(232792560, five:tail_recursion_start()),
   ?assertEqual(232792560, five:recursion_start()),
   ?assertEqual(232792560, five:filter_start()),
   ?assertEqual(232792560, five:map_start()).
+
 
