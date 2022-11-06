bf32f33efadc09b62cee591b629df6b2a170de74
олег <oleg.ru1234567890@mail.ru>
2022-11-06 14:58:56 +0300

    test add for 26

---
+++ task_5/src/test_period.erl
@@ -0,0 +1,19 @@
+-module(test_period).
+-author("олег").
+
+%% API
+-include_lib("eunit/include/eunit.hrl").
+
+%% test five module
+first_test() ->
+  ?assertEqual(983, period:tail_recursion_start()).
+second_test() ->
+  ?assertEqual(983, period:recursion_start()).
+third_test() ->
+  ?assertEqual(983, period:fold_start()).
+four_test() ->
+  ?assertEqual(983, period:map_start()).
+
+
+
+
