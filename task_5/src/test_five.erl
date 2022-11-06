50cec85d3b9843c8ac4e5549b2b096b816e29920
олег <oleg.ru1234567890@mail.ru>
2022-11-06 14:12:57 +0300

    test add

--- task_5/src/test_five.erl
+++ task_5/src/test_five.erl
@@ -5,10 +5,16 @@
 -include_lib("eunit/include/eunit.hrl").
 
 %% test five module
-funct_test() ->
-  ?assertEqual(232792560, five:tail_recursion_start()),
-  ?assertEqual(2520, five:recursion_start()),
-  ?assertEqual(2520, five:filter_start()),
+first_test() ->
+  ?assertEqual(232792560, five:tail_recursion_start()).
+second_test() ->
+  ?assertEqual(2520, five:recursion_start()).
+third_test() ->
+  ?assertEqual(2520, five:filter_start()).
+four_test() ->
   ?assertEqual(2520, five:map_start()).
+
+
+
 
 
